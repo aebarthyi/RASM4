175454515ca27788a2dcad19d87e1823fe9d1bb8
Alex <mechdragon23@gmail.com>
2020-11-15 15:03:33 -0800

    changed amount of memory to malloc

--- source/Add_String_Keyboard.s
+++ source/Add_String_Keyboard.s
@@ -1,6 +1,6 @@
 @ Add_String_keyboard function
 @ Inputs:	r0 = Address of the string to add
-@			r1 = Address of last node
+@		r1 = Address of last node
 @ Outputs:      r0 = Address of the new node in linked list
 @ Purpose:      creates an empty linked list head 
 
@@ -19,39 +19,41 @@ Add_String_keyboard:
 	mov	r6, r0			@ move address of string into r6
 	
 	push	{r1-r8, r10, r11}       @ preserved registers
-	bl	String_Length	@ call string length
-	pop		{r1-r8, r10, r11}       @ pop registers
+	bl	String_Length		@ call string length
+	pop	{r1-r8, r10, r11}       @ pop registers
 	
 	add	r0, r0, #8	@adding 8 to string length
 	mov	r5, r0		@moving length into r5
 
+	add	r0, r0, #4	@increse memory alocation by 4 more bytes
+
 	push	{r1-r8, r10, r11}       @ preserved registers
 	bl 	malloc			@ allocate memory to store string and next/prev addresses
-	pop		{r1-r8, r10, r11}       @ pop registers
+	pop	{r1-r8, r10, r11}       @ pop registers
 
 	mov	r4, #0		@setting temp count to 0
-	mov	r7, #0		@setting r7 to 0
+	mov	r7, #0x00000000	@setting r7 to 0
 				
-	ldr		r3, [r1]		@ load address of last node
-	str		r3, [r0]		@storing prev address to our new node
+	ldr	r3, [r1]	@ load address of last node
+	str	r3, [r0]	@storing prev address to our new node
 	
-	str		r0, [r1, #4]	@store our current node address to prev node next
-	str		r7, [r0, #4]	@store next of our current to be null
+	str	r0, [r1, #4]	@store our current node address to prev node next
+	str	r7, [r0, #4]	@store next of our current to be null
 	
 	mov	r2, #0		@setting count to 0
-	mov r4, #8		@setting offset for copy
+	mov 	r4, #8		@setting offset for copy
 
 copy:
 	ldrb	r3, [r6, r2]	@loading string byte offset by r4
 	strb	r3, [r0, r4]	@storing string byte in new node offset by r2
 
 	add	r2, #1		@increment counter by 1
-	add r4, #1		@increment offset of node by 1
+	add 	r4, #1		@increment offset of node by 1
 
 	cmp	r2, r5		@compare count to total length
 	blt	copy		@if less then jump to copy
 
-	pop	{lr}					@preservs the link register for recursion
+	pop	{lr}			@preservs the link register for recursion
 	pop	{sp}                    @ pop stack pointer
 	pop	{r4-r8, r10, r11}       @ pop the preserved regiesters for aapcs
 
