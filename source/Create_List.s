626b2a45d177265e676a3dc5edfd76e753c64e47
Alex <mechdragon23@gmail.com>
2020-11-15 13:04:42 -0800

    fixed so head node is initilized with 0's

--- source/Create_List.s
+++ source/Create_List.s
@@ -15,6 +15,10 @@ Create_List:
     
     mov	r0, #8			@ move 8 into r0 
     bl 	malloc			@ allocate memory for the first node, the head with a null prev and null next
+
+    add r0, #4			@increment r0 by 
+    mov r4, #0x00000000		@move all 0's into r4
+    str r4, [r0]		@store all 0's into next
     
     pop	{sp}                    @ pop stack pointer
     pop	{r4-r8, r10, r11}       @ pop the preserved regiesters for aapcs
