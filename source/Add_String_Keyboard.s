bc1e6621a715ffcbe240fc5b8cd96acdd8b75017
Andrew <sonic757RAGEquit@gmail.com>
2020-11-15 13:14:42 -0800

    fixed up add string

--- source/Add_String_Keyboard.s
+++ source/Add_String_Keyboard.s
@@ -1,6 +1,6 @@
 @ Add_String_keyboard function
 @ Inputs:	r0 = Address of the string to add
-@		r1 = Address of last node
+@			r1 = Address of last node
 @ Outputs:      r0 = Address of the new node in linked list
 @ Purpose:      creates an empty linked list head 
 
@@ -16,57 +16,45 @@ Add_String_keyboard:
 	push 	{sp}                   @ push stack pointer
 	push	{lr}			@preserve the link register for recursion
     
-	mov	r6, r0		@ move address of string into r6
+	mov	r6, r0			@ move address of string into r6
+	
+	push	{r1-r8, r10, r11}       @ preserved registers
 	bl	String_Length	@ call string length
+	pop		{r1-r8, r10, r11}       @ pop registers
 	
 	add	r0, r0, #8	@adding 8 to string length
 	mov	r5, r0		@moving length into r5
 
 	push	{r1-r8, r10, r11}       @ preserved registers
 	bl 	malloc			@ allocate memory to store string and next/prev addresses
-	pop	{r1-r8, r10, r11}       @ pop registers
+	pop		{r1-r8, r10, r11}       @ pop registers
 
-	mov	r2, #0		@setting count to 0
 	mov	r4, #0		@setting temp count to 0
-	mov	r7, #00		@setting r7 to 00
-
-copyaddressprev:				
-	ldrb	r3, r1, r2	@needs HELP fix?
-	strb	r3, [r0, r2]	@storing r2 into r5 + r1
-
-	add	r2, #1		@increment counter by 1
-
-	cmp	r2, #4		@compare counter to 4
-	blt	copyaddressprev	@if less then jump to copy	
-
-copyaddressnext:				
-	ldrb	r3, r0, r4	@needs HELP fix?
-	strb	r3, [r1, r2]	@storing r2 into r5 + r1
-
-	strb	r7, [r0, r2]	@writing 0's into current node's next
-
-	add	r2, #1		@increment counter by 1
-	add	r4, #1		@increment temp counter by 1
-
-	cmp	r2, #8		@compare counter to 8
-	blt	copyaddressnext	@if less then jump to copy
-
-	mov	r4, #0		@reset temp counter to 0
+	mov	r7, #0		@setting r7 to 0
+				
+	ldr		r3, [r1]		@ load address of last node
+	str		r3, [r0]		@storing prev address to our new node
+	
+	str		r0, [r1, #4]	@store our current node address to prev node next
+	str		r7, [r0, #4]	@store next of our current to be null
+	
+	mov	r2, #0		@setting count to 0
+	mov r4, #8		@setting offset for copy
 
 copy:
-	ldrb	r3, [r6, r4]	@loading string byte offset by r4
-	strb	r3, [r0, r2]	@storing string byte in new node offset by r2
+	ldrb	r3, [r6, r2]	@loading string byte offset by r4
+	strb	r3, [r0, r4]	@storing string byte in new node offset by r2
 
 	add	r2, #1		@increment counter by 1
-	add	r4, #1		@increment temp counter by 1
+	add r4, #1		@increment offset of node by 1
 
 	cmp	r2, r5		@compare count to total length
 	blt	copy		@if less then jump to copy
 
-	pop	{lr}			@preservs the link register for recursion
+	pop	{lr}					@preservs the link register for recursion
 	pop	{sp}                    @ pop stack pointer
 	pop	{r4-r8, r10, r11}       @ pop the preserved regiesters for aapcs
 
-	bx      lr    	            	@ back to the main where it is called
+	bx      lr    	            @ back to the main where it is called
 
     .end
