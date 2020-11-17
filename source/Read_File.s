87c97af7b2ba732468457363e299f788af97100c
Andrew <sonic757RAGEquit@gmail.com>
2020-11-16 22:36:27 -0800

    updated READ_FILE

--- source/Read_File.s
+++ source/Read_File.s
@@ -20,24 +20,22 @@ Read_File:
 	ldr		r1, =szByte			@ load character buffer
 	mov		r2, #1				@ set bytes to read to 1
 	mov		r7, #3				@ set syscall to read
+	ldr		r5, =szEmp			@ load empty buffer for line
 	mov		r4, #0				@ set our counter up
 	
 CharCheck:
 	svc		0					@ call to read in one character
 	cmp		r0, #1				@ check if a byte was read or not
-	blt		ReadIn				@ go to read in if end of file found
-	cmp		r1, #10				@ check if newline character found
+	blt		exit				@ go to read in if end of file found
+	mov		r0, r6				@ restore file descriptor
+	ldrb	r3, [r1]			@ dereference the byte
+	cmp		r3, #10				@ check if newline character found
+	strneb	r3, [r5, r4]		@ store in the buffer if not newline
 	addne	r4, #1				@ increment counter if not found
 	bne		CharCheck			@ back to top of loop
-	
-ReadIn:
-	mov		r0, r6				@ restore file descriptor
-	ldr		r1, =szEmp			@ move buffer to be read into
-	mov		r2, r4				@ put our line length into r2
-	svc  	0					@ call to read into the buffer
-	
-	ldr		r0, =szEmp			@ get our address to read in line
-	
+
+exit:
+	mov		r0, r5				@ return address of read in line
 	pop		{sp}				@ preserve stack pointer
 	pop		{r4-r8, r10, r11}	@ preserve registers
 	
