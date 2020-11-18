ed4a27f0dd4c87ac32662e3f66101695f96b5783
Alex <mechdragon23@gmail.com>
2020-11-17 16:50:53 -0800

    fixed the exit delete function to work using delete string

--- source/Exit_Delete.s
+++ source/Exit_Delete.s
@@ -15,19 +15,27 @@ Exit_Delete:
 	push 	{sp}                   @ push stack pointer
 	push	{lr}			@preserve the link register for recursion
 
-nextaddress:
-	ldr	r1, [r0, #4]	@get next address
-	mov r3, r1			@move next node to r3
-	
-	push	{r1-r8, r10, r11}       @ preserved registers
-	bl		free					@ frees the memory at the address in r0 (node to delete)
-	pop		{r1-r8, r10, r11}       @ pop registers
-	
-	mov	r0, r3						@ restore next node
-	cmp	r0, #0						@ check if nullptr
-	bne nextaddress					@ loop
-	
+	mov	r8, r0		@copy the head node to r8
+loop:
+	mov	r1, #1		@setting node to delete to 1
+	mov	r0, r8		@mov head node into r0
+
+	bl	Delete_String	@calls delete string
+	cmp	r1, #1		@compare if last node was deleted
+	beq	end		@jump to end
+
+	cmp	r0, #0		@compare if the delete string was ran
+	beq	end		@jump to end
+
+	b	loop		@jump back to loop
+		
 end:
+	mov	r0, r8		@mov head back to r0 for deletion
+
+	push	{r1-r8, r10, r11}       @ preserved registers
+	bl	free			@frees the memory at the address in r0 (node to delete)
+	pop	{r1-r8, r10, r11}       @ pop registers
+	
 	pop	{lr}					@preservs the link register for recursion
 	pop	{sp}                    @ pop stack pointer
 	pop	{r4-r8, r10, r11}       @ pop the preserved regiesters for aapcs
